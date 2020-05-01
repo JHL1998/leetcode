7d62741aab4590ce2184b322598050b150bd8b92
罗建华 <luojianhua@luojianhuadeMacBook-Air.local>
2020-05-01 16:59:39 +0800

    5-1

---
+++ src/Tree/binaryTreeInorderTraversal.java
@@ -0,0 +1,56 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94、中序遍历是一种非常有用的性质，二分搜索树的中序遍历出来是一个递增的序列
+ * 递归
+ */
+public class binaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        inOrder(root,res);
+        return res;
+    }
+
+    private void inOrder(TreeNode node,List<Integer> res){
+        if(node==null) return ;
+        inOrder(node.left,res);
+        res.add(node.val);
+        inOrder(node.right,res);
+
+    }
+}
+
+/**
+ * 利用栈实现模拟非递归中序遍历
+ *
+ *
+ */
+ class binaryTreeInorderTraversal2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer>res=new ArrayList<>();
+        Stack<TreeNode>stack=new Stack<>();
+        TreeNode cur=root;
+        //先判断当前结点是否存在，若存在则将该节点放入栈中，再将当前结点设置为结点的左孩子，
+        while(cur!=null||!stack.isEmpty()){
+            if(cur!=null){
+                stack.push(cur);
+                cur=cur.left;
+            }else{
+                // 若不存在则取栈顶元素为cur，当且仅当栈空cur也为空，循环结束。
+                cur=stack.pop();
+                res.add(cur.val);
+                cur=cur.right;
+            }
+        }
+        return res;
+
+    }
+
+
+}
+
---
+++ src/Tree/invertBinaryTree.java
@@ -0,0 +1,44 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 226、递归实现
+ */
+public class invertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root){
+        if(root==null) return null;
+        //保存left指针
+        TreeNode leftTree=root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(leftTree);
+
+       return root;
+
+    }
+}
+
+/**
+ *BFS/层序遍历， 直接交换左右两个节点
+ */
+ class invertBinaryTree2 {
+
+    public TreeNode invertTree(TreeNode root){
+        if(root==null) return null;
+       Queue<TreeNode> queue=new LinkedList<>();
+       queue.add(root);
+       while(!queue.isEmpty()){
+           TreeNode cur=queue.poll();
+           TreeNode temp=cur.right;
+           cur.right=cur.left;
+           cur.left=temp;
+           if(cur.left!=null) queue.add(cur.left);
+           if(cur.right!=null) queue.add(cur.right);
+       }
+       return root;
+    }
+}
+
+
